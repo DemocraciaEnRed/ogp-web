9c9fc7e44253bd58242ad55d092cf978c5edfb46
devhormiga <devhormiga@gmail.com>
2022-10-17 16:08:14 -0300

    se agrego el carousel a desktop (mobile usa la version de texto plano por el momento). Se acomodo el calendario (modo agenda en mobile, y las tab ahora son clickeables en desktop)

---
+++ javascript/carusel.js
@@ -0,0 +1,76 @@
+const slides = document.getElementById('carousel');
+const rightBtn = document.getElementById('right-btn');
+const leftBtn = document.getElementById('left-btn');
+
+let contenido = document.getElementById('slide1');
+let totalSlides = slides.children.length;
+let position = 0
+let anterior = '';
+let siguiente = '';
+
+if (totalSlides != 0){
+    // inicializar carusel
+    console.log('init carousel!!!')
+   
+  
+}
+
+
+leftBtn.style.visibility = 'hidden';
+// cequeos para el desarrollo
+console.log('recuerda borrar estos ocnsolLog');
+console.log(slides);
+console.log(totalSlides);
+console.log(slides.childNodes)
+console.log('++++++++++++++++++++++++');
+
+const moveRight = () => {
+
+    //     return;
+    if (position < totalSlides){
+
+
+        actual = document.getElementById(slides.childNodes[position].id);
+        actual.style.display= "none";
+        position++;
+
+        siguiente = document.getElementById(slides.childNodes[position].id);
+
+        siguiente.style.display= "block";
+
+        leftBtn.style.visibility = 'visible';
+        if (position == 0){
+            leftBtn.style.visibility = 'hidden';
+        }
+         if(position == 3){
+            rightBtn.style.visibility = 'hidden';
+        }
+    }
+}
+
+const moveLeft = () => {
+    if (position < totalSlides){
+
+        console.log('estado',position);
+
+        actual = document.getElementById(slides.childNodes[position].id);
+        actual.style.display= "none";
+        position--;
+        console.log('next',position);
+
+        siguiente = document.getElementById(slides.childNodes[position].id);
+
+        siguiente.style.display= "block";
+
+        leftBtn.style.visibility = 'visible';
+        if (position == 0){
+            leftBtn.style.visibility = 'hidden';
+        }
+         if(position < totalSlides -1 ){
+            rightBtn.style.visibility = 'visible';
+        }
+    }
+}
+
+rightBtn.addEventListener("click", moveRight);
+leftBtn.addEventListener("click", moveLeft);
